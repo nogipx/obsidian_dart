163f00c376b2651dd2d368118d42c9db876faccd
nogipx <nogipx@gmail.com>
2026-03-26 16:07:08 +0300

    upd deps

--- lib/src/friendly/events.dart
+++ lib/src/friendly/events.dart
@@ -4,22 +4,21 @@ import 'dart:js_interop';
 import 'dart:js_util' as jsu;
 
 import 'package:obsidian_dart/obsidian_dart.dart';
-import 'package:rpc_dart/rpc_dart.dart';
 
 class VaultEvents {
-  VaultEvents(
-    this.plugin, {
-    RpcLogger? log,
-  }) : _log = log?.child('VaultEvents');
+  VaultEvents(this.plugin);
 
-  final RpcLogger? _log;
   final PluginHandle plugin;
 
   Stream<VaultEvent> get all => _all.stream;
-  Stream<VaultEvent> get created => _all.stream.where((e) => e.type == VaultEventTypes.create);
-  Stream<VaultEvent> get modified => _all.stream.where((e) => e.type == VaultEventTypes.modify);
-  Stream<VaultEvent> get deleted => _all.stream.where((e) => e.type == VaultEventTypes.delete);
-  Stream<VaultEvent> get renamed => _all.stream.where((e) => e.type == VaultEventTypes.rename);
+  Stream<VaultEvent> get created =>
+      _all.stream.where((e) => e.type == VaultEventTypes.create);
+  Stream<VaultEvent> get modified =>
+      _all.stream.where((e) => e.type == VaultEventTypes.modify);
+  Stream<VaultEvent> get deleted =>
+      _all.stream.where((e) => e.type == VaultEventTypes.delete);
+  Stream<VaultEvent> get renamed =>
+      _all.stream.where((e) => e.type == VaultEventTypes.rename);
 
   final _all = StreamController<VaultEvent>.broadcast();
   final _refs = <JSObject>[];
@@ -27,14 +26,15 @@ class VaultEvents {
 
   void attach() {
     if (_attached) {
-      _log?.debug('Already attached, skipping');
       return;
     }
     _attached = true;
-    _log?.debug('Attaching to vault events...');
     final vault = plugin.app.vault;
 
-    void register(String event, VaultEvent Function(List<Object?> args) mapper) {
+    void register(
+      String event,
+      VaultEvent Function(List<Object?> args) mapper,
+    ) {
       final handler = jsu.allowInterop((JSAny a, [JSAny? b]) {
         if (_all.isClosed) {
           return;
@@ -42,10 +42,6 @@ class VaultEvents {
         final args = <Object?>[a, b];
         final vaultEvent = mapper(args);
         _all.add(vaultEvent);
-        _log?.debug(
-          'Received raw event: $event \n'
-          'Mapped to VaultEvent: type=${vaultEvent.type} path=${vaultEvent.file.path}',
-        );
       });
       // ignore: invalid_runtime_check_with_js_interop_types
       final ref = vault.on(event, handler as JSFunction);
@@ -53,23 +49,13 @@ class VaultEvents {
       _refs.add(ref);
     }
 
-    register(
-      'create',
-      (args) => VaultEvent.create(_asFile(args[0])),
-    );
-    register(
-      'modify',
-      (args) => VaultEvent.modify(_asFile(args[0])),
-    );
-    register(
-      'delete',
-      (args) => VaultEvent.delete(_asAbstractFile(args[0])),
-    );
+    register('create', (args) => VaultEvent.create(_asFile(args[0])));
+    register('modify', (args) => VaultEvent.modify(_asFile(args[0])));
+    register('delete', (args) => VaultEvent.delete(_asAbstractFile(args[0])));
     register(
       'rename',
       (args) => VaultEvent.rename(_asAbstractFile(args[0]), args[1] as String?),
     );
-    _log?.debug('All event handlers registered successfully');
   }
 
   Future<void> dispose() {
@@ -114,11 +100,14 @@ class WorkspaceEvents {
 class VaultEvent {
   VaultEvent._(this.type, this.file, {this.oldPath});
 
-  factory VaultEvent.create(TFileHandle file) => VaultEvent._(VaultEventTypes.create, file);
+  factory VaultEvent.create(TFileHandle file) =>
+      VaultEvent._(VaultEventTypes.create, file);
 
-  factory VaultEvent.modify(TFileHandle file) => VaultEvent._(VaultEventTypes.modify, file);
+  factory VaultEvent.modify(TFileHandle file) =>
+      VaultEvent._(VaultEventTypes.modify, file);
 
-  factory VaultEvent.delete(TAbstractFileHandle file) => VaultEvent._(VaultEventTypes.delete, file);
+  factory VaultEvent.delete(TAbstractFileHandle file) =>
+      VaultEvent._(VaultEventTypes.delete, file);
 
   factory VaultEvent.rename(TAbstractFileHandle file, String? oldPath) =>
       VaultEvent._(VaultEventTypes.rename, file, oldPath: oldPath);
@@ -138,4 +127,5 @@ class VaultEventTypes {
 }
 
 TFileHandle _asFile(Object? value) => TFileHandle(value! as JSObject);
-TAbstractFileHandle _asAbstractFile(Object? value) => TAbstractFileHandle(value! as JSObject);
+TAbstractFileHandle _asAbstractFile(Object? value) =>
+    TAbstractFileHandle(value! as JSObject);
